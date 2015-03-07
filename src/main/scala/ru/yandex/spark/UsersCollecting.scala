e45d454042e1f2b663f5fb5774abf77ec47e609d
sslavian812 <sslavian812@yandex.ru>
2015-03-07 22:35:30 +0300

    storing one user per file

--- src/main/scala/ru/yandex/spark/UsersCollecting.scala
+++ src/main/scala/ru/yandex/spark/UsersCollecting.scala
@@ -5,6 +5,9 @@ import breeze.optimize.MaxIterations
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
+import java.nio.file.{Paths, Files}
+import java.nio.charset.StandardCharsets
+
 
 object UsersCollecting {
 
@@ -15,7 +18,7 @@ object UsersCollecting {
     if(args.length != 0)
       args(0)
     else
-      "D:\\tmp\\downloading\\"
+      "D:\\tmp\\downloading"
 
     val iterations : Int=
     if(args.length == 2)
@@ -26,7 +29,6 @@ object UsersCollecting {
     var idsToLoad = sc.parallelize(List("1", "2"))
     var loadedIds = sc.parallelize(List[String]())
 
-
     for( i <- 1 until iterations) {
       val currentUsers = idsToLoad.map(u => VkUser.addExtraInformation(User(u)))
       // load Users
@@ -35,12 +37,15 @@ object UsersCollecting {
       val friends = currentUsers.map(u => u.friends).reduce((l1, l2) => List.concat(l1, l2)).distinct
       idsToLoad = sc.parallelize(friends.map(x => "" + x)).subtract(loadedIds)
       // update downloading queue
-
-      val currentUsersAsJson = currentUsers.map(u => pretty(VkUser.getJsonRepresentation(u)))
-      currentUsersAsJson.saveAsTextFile(storageDirectory + i)
+      
+      currentUsers.foreach(u => storeUser(u, storageDirectory))
       // storing on disc
     }
 
     println("success!")
+  }
+
+  def storeUser(user: User, path : String): Unit = {
+    Files.write(Paths.get(path + "\\" + user.id + ".txt" ), pretty(VkUser.getJsonRepresentation(user)).getBytes(StandardCharsets.UTF_8))
   }
 }
