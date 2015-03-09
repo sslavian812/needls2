510f975654ab52f8b156e6bf764b4a67a275599c
sslavian812 <sslavian812@yandex.ru>
2015-03-09 13:35:41 +0300

    some improvements

--- src/main/scala/ru/yandex/spark/UsersCollecting.scala
+++ src/main/scala/ru/yandex/spark/UsersCollecting.scala
@@ -5,7 +5,9 @@ import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
 
-
+/**
+ * usage: path iterations start_user_id
+ */
 object UsersCollecting {
 
   implicit val formats = DefaultFormats
@@ -13,28 +15,34 @@ object UsersCollecting {
   def main(args: Array[String]): Unit = {
 
     val storageDirectory =
-    if(args.length != 0)
+    if(args.length >= 1)
       args(0)
     else
       "D:\\tmp\\downloading"
 
     val iterations : Int=
-    if(args.length == 2)
+    if(args.length >= 2)
       args(1).toInt
     else
-      2
+      1
 
-    var idsToLoad = sc.parallelize(List("1", "2"))
+    val startId : String =
+    if(args.length >= 3)
+      args(2)
+    else
+      "1"
+
+    var idsToLoad = sc.parallelize(List(startId))
     var loadedIds = sc.parallelize(List[String]())
 
-    for( i <- 1 until iterations) {
+    for( i <- 0 until iterations) {
 
         val currentUsers = idsToLoad.map(u => VkUser.addExtraInformation(User(u)))
         // load Users
 
         loadedIds = loadedIds.union(currentUsers.map(u => u.id))
-        val friends = currentUsers.map(u => u.friends).reduce((l1, l2) => List.concat(l1, l2)).distinct
-        idsToLoad = sc.parallelize(friends.map(x => "" + x)).subtract(loadedIds)
+        val friends = currentUsers.flatMap(u => u.friends).distinct()
+        idsToLoad = friends.map(x => "" + x).subtract(loadedIds)
         // update downloading queue
 
         currentUsers.foreach(u => VkUser.storeUser(u, storageDirectory))
