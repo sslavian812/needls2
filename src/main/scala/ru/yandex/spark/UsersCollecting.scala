3fa86f42a0d3a65f93a5035f1e607f8b27f2c5d4
sslavian812 <sslavian812@yandex.ru>
2015-04-18 15:13:27 +0300

    user collecting process refactored, but undesired behaviour still exists

--- src/main/scala/ru/yandex/spark/UsersCollecting.scala
+++ src/main/scala/ru/yandex/spark/UsersCollecting.scala
@@ -5,63 +5,66 @@ import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
 
-/**
- * usage: path iterations start_user_id
- */
 object UsersCollecting {
 
   implicit val formats = DefaultFormats
   val usersPerIteration = 10
 
+  /**
+   * VM options:
+   *     -Dpath=path - path to local filesystem, if it's necessary to store data on disc
+   *     -DstartId=id - id of user, which from will bfs start
+   *     -Dcount=number - number of users to be loaded, (will be rounded by userPerIteration constant)
+   * @param args
+   */
   def main(args: Array[String]): Unit = {
-
-    val storageDirectory =
-    if(args.length >= 1)
-      args(0)
-    else
-      "D:\\tmp\\downloading"
+    var startId : String          = System.getProperty("startId")
+    val count : String            = System.getProperty("count")
+    val storageDirectory : String = System.getProperty("path")
 
     val iterations : Int=
-    if(args.length >= 2)
-      args(1).toInt
+    if(count != null)
+      (count.toInt) / usersPerIteration + 1
     else
       1
 
-    val startId : String =
-    if(args.length >= 3)
-      args(2)
-    else
-      "1"
+    if(startId == null || "".equals(startId))
+      startId = "1"
 
-    var idsToLoad = sc.parallelize(List(startId))
-    var loadingQueue = sc.parallelize(List[String]())
-    var loadedIds = sc.parallelize(List[String]())
+    var mainLoadingQueue = sc.parallelize(List[String]())
+    var alreadyLoadedIds = sc.parallelize(List[String]())
+    var loadOnIteration = sc.parallelize(List(startId))
+
     ElasticSearchHelper.init()
 
 
     for( i <- 0 until iterations) {
-      val currentUsers = idsToLoad.map(u => VkUser.addExtraInformation(User(u)))
+      println("=======" + i + "-th iteration")
+      println("MLQ.size = " + mainLoadingQueue.count())
+
+      val currentUsers = loadOnIteration.map(u => VkUser.addExtraInformation(User(u)))
+      alreadyLoadedIds = alreadyLoadedIds.union(currentUsers.map(_.id))
       // load Users
 
-      loadedIds = loadedIds.union(currentUsers.map(_.id))
-      println("=======" + i + "-th iteration")
+      val friendsIds = currentUsers.flatMap(_.friends).distinct()
+      println("friends.size = " + friendsIds.count())
 
-      val friends = currentUsers.flatMap(_.friends).distinct()
-      loadingQueue = friends.map(_.toString).subtract(loadedIds)
+      mainLoadingQueue = friendsIds.map(_.toString).subtract(alreadyLoadedIds)
+      println("MLQ.size = " + mainLoadingQueue.count())
 
-      println("####### before substracting")
-      println("total: "+ loadingQueue.count())
-      loadingQueue = loadingQueue.subtract(idsToLoad)
-      println("####### after substracting")
-      println("total: "+loadingQueue.count())
+      loadOnIteration = sc.parallelize(mainLoadingQueue.take(usersPerIteration))
+      println("LOI.size = " + loadOnIteration.count())
 
-      idsToLoad = sc.parallelize(loadingQueue.take(usersPerIteration))
+      mainLoadingQueue = mainLoadingQueue.subtract(loadOnIteration)
+      println("MLQ.size = " + mainLoadingQueue.count())
+
       // update downloading queue
 
-      //currentUsers.foreach(u => VkUser.storeUser(u, storageDirectory))
       currentUsers.foreach(u =>  ElasticSearchHelper.addUser(u))
+      if(storageDirectory != null)
+        currentUsers.foreach(u => VkUser.storeUser(u, storageDirectory))
 
-      println(usersPerIteration + " more users added to index")
+      println((if (i==0) "1" else usersPerIteration) + " more users added to index")
     }
 
     println("success!")
